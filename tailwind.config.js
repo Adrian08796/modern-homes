b845dcf4d7a5c092658f76dd71d080b777c106eb
Adrian.V <adrian.radu.vasiu@students.beaminstitute.org>
2024-02-16 11:30:41 +0100

    inititial commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        xs: "300px",
+        // => @media (min-width: 640px) { ... }
+      },
+
+      colors: {
+        menuRed: "rgb(200,28,46)",
+        navBg: "rgb(49,49,49)",
+        pageBg: "rgb(24,27,29)",
+      },
+
+      fontSize: {
+        customFont: "53pt",
+      },
+
+      spacing: {
+        Adrian: "300px",
+      },
+    },
+  },
+  plugins: [],
+};
